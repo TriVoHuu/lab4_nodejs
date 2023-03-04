2e473aff55befaa8e7d18fbc59b5fb097d4f6a71
TriVoHuu <125641072+TriVoHuu@users.noreply.github.com>
2023-03-04 17:53:11 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-require('dotenv').config();
-const express = require('express');
-const {engine, create} = require("express-handlebars");
-const users = require('./users/index');
+const express = require('express')
+const bodyParser = require('body-parser')
+const {engine, create} = require('express-handlebars')
+const users = require('./users/index')
 
-const host = process.env.HOST;
-const port = process.env.PORT;
+const app = express()
 
-const app = express();
-app.set('view engine', 'handlebars');
+app.set('view engine','ejs')
+app.use(bodyParser.urlencoded({extended: false}))
+
 app.engine('handlebars', engine({
-    layoutsDir: __dirname+'/views/layouts',
-    helpers: {
-        json: function(context) {return JSON.stringify(context);},
+    layoutDis: __dirname + '/views',
+    helper: {
+        json: function(context) { return JSON.stringify(context);},
     }
-}));
+}))
 
-app.get('/', async (req, res) => {
-    const params = req.query;
-    const userData = await users.getUsers(Number.parseInt(params.page));
-
-    res.render('main', {layout: 'index', data: {users: userData.data, pagination}})
-});
-
-app.get('/users/:userId', async (req,res) => {
-    const {userId} = req.params;
-    const usern= await users.getUser(userId);
-    console.log(user);
+app.get('/',async (req,res) => {
+    const param = req.query;
+    const userData = await users.getUsers(Number.parseInt(param.page))
+    console.log(userData)
+    res.render('user', {users: userData.data})
 })
+
+app.use((req, res) => {
+    res.set('Content-Type','text/html')
+    res.end("liên kết này không được hỗ trợ")
+})
+
+
+app.listen(8080, () => console.log('http://localhost:8080'))
